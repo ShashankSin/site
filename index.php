a2d7c44d14af2d5d938961a7e890ba3413dd5cc2
Shashank Singh <95364209+ShashankSin@users.noreply.github.com>
2024-01-25 21:04:55 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Site</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<!-- Nav start -->
+	<nav>
+		<div class="center">
+			<ul>
+				<li><a href="#" class="active-menu">Home</a></li>
+				<li><a href="#">Welcome</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">About us</a></li>
+				<li><a href="#">Contact us</a></li>
+				<li><a href="#"> More about us</a></li>
+			</ul>
+			<div class="clear"></div>
+		</div>
+	</nav>
+	<!-- Nav end -->
+
+	<!-- Header start -->
+	<!-- <header class="don">
+	  <div class="center">
+	  		<a href="#" target="_blank" class="logo"><img src="images/JBU.png" alt="JBU energy"></a>
+	  		<div class="social">
+	  			<a href="#"><img src="images/fa.png" alt="facebook"></a>
+	  			<a href="#"><img src="images/tt.png" alt="Twetter"></a>
+	  			<a href="#"><img src="images/int.png" alt="instagram"></a>
+	  			<a href="#"><img src="images/gg.png" alt="google"></a>	
+	  		</div>
+	  		<div class="clear"></div>
+	  </div>	
+	</header> -->
+	<!-- Header end -->
+
+	<!-- <div class="banner">
+		<div class="center">
+			<div class="battery">
+				<img src="images/battery.png" alt="Battery">
+			</div>
+			<div class="ourproduct">
+				<img src="images/our.png" alt="our products">
+				<ul>
+					<li>Automotive Batteries</li>
+					<li>Inverter Batteries</li>
+					<li>Tubular Batterirs</li>
+					<li>Two Wheeler Batteries</li>
+					<li>Solar Batteries</li>
+				</ul>
+				<a href="#" class="btnn">Read more</a>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div> -->
+	
+	<!-- <main>
+		<div class="center">
+			<h1>Who are We</h1>
+			<p>Offering unique level of product and service excellence, we are an industry innovator exhibiting unbeatable performance since our inception. <strong>Shree Jagdamba Battery Udhyog</strong> was established in the year 2006 and since then it has been playing a forerunner role in the industry with an unchallenged leadership.</p>
+			<a href="#" class="btnn btn-grey">Read more</a>
+		</div>
+	</main> -->
+
+	<!-- <section>
+		<div class="center">
+
+			<div class="feature">
+				<img src="images/iso.png">
+				<span>
+					<strong>Quality</strong>
+					<small>
+						ISO 9001:2008<br>
+						Company
+					</small>
+				</span>
+			</div>
+
+			<div class="feature bor">
+				<img src="images/hands.png">
+				<span>
+					<strong>CSR</strong>
+					<small>
+						Corporate Social<br>
+						Responsibility
+					</small>
+				</span>
+			</div>	
+
+			<div class="feature">
+				<img src="images/bag.png">
+				<span>
+					<strong>Career</strong>
+					<small>
+						Job opportunities<br>
+						at JBU Energy
+					</small>
+				</span>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section> -->
+
+	<!-- <footer>
+		<div class="center fe">
+			<div class="fleft">
+				<h3>Shree Jagdamba Battery Udhyog</h3>
+				<ul class="bar">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Introduction</a></li>
+					<li><a href="#">Mission, Vision & Core value</a></li>
+					<li><a href="#">Our Team</a></li>
+					<li><a href="#">Product Range</a></li>
+				</ul>
+				<ul class="cr">
+					<li><a href="#">CRS</a></li>
+					<li><a href="#">Research</a></li>
+					<li><a href="#">Dealer’s network</a></li>
+					<li><a href="#">FAQs</a></li>
+					<li><a href="#">Contact us</a></li>
+				</ul>
+			</div>
+			<div class="fright">
+				<div class="hoffice">
+					<h4>Head office:</h4>
+					<p>
+						Gaur-8, Rautahat, Nepal, Phone: <a href="callto:97755520455">+977-55-520455</a>,
+						<a href="callto:97755520051">520051</a> <br>
+						Fax: <a href="callto:97755520329">+977-55-520329</a>, E-mail: <a href="mailto:info@jubuenergy.com">info@jubuenergy.com</a>
+					</p>
+				</div>
+				<div>
+					<div class="boffice bof">
+						<h5>Branch Office (Kathmandu)</h5>
+						<p>
+							3rd Floor, KKM Building, Tripureshwor<br>
+							Kathmandu, Nepal, Phone: <a href="callto:97714262627">+977 1 4262627</a><br>
+							Fax: <a href="callto:97714258802">+977 1 4258802</a>, E-mail: <a href="mailto:jbuktm@jbuenergy.com">jbuktm@jbuenergy.com</a>
+						</p>	
+					</div>
+					<div class="boffice">
+						<h5>Branch Office (Birgunj)</h5>
+						<p>
+							Adarsh Nagar, Birgunj, Nepal<<br>
+							Phone: <a href="callto:977985525530">+977 985525530</a><br>
+							E-mail: <a href="mailto:jbubirgung@jbuenergy.com">jbubirgung@jbuenergy.com</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</footer> -->
+
+	<!-- <header class="tea">
+		<div class="center">
+			<p class="fas">
+				Copyright © 2013 JBU Energy, All Rights Reserved  |  Designed by:
+			</p>
+			<h3>An ISO 9001:2008 Company</h3>
+		</div>
+		<div class="clear"></div>
+	</header> -->
+</body>
+</html>
